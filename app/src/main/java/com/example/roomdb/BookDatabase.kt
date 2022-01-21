3ff240741ee5744c0bc3276db788690285fc997a
isilay-subasi <isil.subasi19@gmail.com>
2022-01-21 15:35:48 +0300

    TestDB

--- app/src/main/java/com/example/roomdb/BookDatabase.kt
+++ app/src/main/java/com/example/roomdb/BookDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.roomdb
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 
 @Database(entities = [Book::class],version = 1)
-abstract class BookDatabase {
+abstract class BookDatabase : RoomDatabase() {
     abstract fun bookDao() :BookDao
 
 }
