9077f76b8ef2e8c4148da912aa22792577d8d56b
isilay-subasi <isil.subasi19@gmail.com>
2022-01-21 14:58:36 +0300

    BookDao Interface

---
+++ app/src/main/java/com/example/roomdb/BookDao.kt
@@ -0,0 +1,22 @@
+package com.example.roomdb
+
+import androidx.room.*
+
+
+@Dao
+interface BookDao {
+
+    @Insert
+    suspend fun insertBook(book: Book)
+
+
+    @Query("SELECT * FROM books_table")
+    fun getAllBooks(): List<Book>
+
+    @Update
+    suspend fun updateBook(book: Book)
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+
+}
